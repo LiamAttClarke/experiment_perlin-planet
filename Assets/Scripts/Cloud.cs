92e2137fe996b2694606b5a1a408190bf235d185
Liam Atticus Clarke <liam.att.clarke@gmail.com>
2015-01-03 19:34:06 -0500

    Added Noise To Cloud Mesh
    Added 3D Perlin Noise to cloud mesh.

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -3,15 +3,29 @@ using System.Collections;
 
 namespace Universe {
 	public class Cloud : MonoBehaviour {
+
 		GameObject planet;
-		Vector3 planetCloudVect;
-		Vector3 planetCameraVect;
+		Vector3 planetCloudVect, planetCameraVect;
 		float cloudScale;
+		float cloudOverHang = 0.75f;
+
 		// Use this for initialization
 		void Start () {
 			planet = GameObject.Find("Planet");
 			transform.LookAt (transform.position * 2);
-//			ResizeCloud ();
+			int seed = Random.Range (0, 100);
+			PerlinNoise cloudNoise = new PerlinNoise(seed);
+			Mesh cloudMesh = gameObject.GetComponent<MeshFilter> ().mesh;
+			Vector3[] cloudVerts = cloudMesh.vertices;
+			float spread = 0.1f;
+			float amplitude = 0.75f;
+			for (int i = 0; i < cloudVerts.Length; i++) {
+				float cloudMap = amplitude * (float)cloudNoise.Noise ((double)cloudVerts[i].x / spread, (double)cloudVerts[i].y / spread, (double)cloudVerts[i].z / spread) + 1.0f;
+				cloudVerts[i] *= cloudMap;
+			}
+			cloudMesh.vertices = cloudVerts;
+			cloudMesh.RecalculateNormals ();
+			cloudMesh.Optimize ();
 		}
 		
 		void Update () {
@@ -21,7 +35,7 @@ namespace Universe {
 		void ResizeCloud () {
 			planetCloudVect = transform.position - planet.transform.position;
 			planetCameraVect = planet.transform.position - Camera.main.transform.position;
-			cloudScale = Mathf.Clamp (Vector3.Dot(planetCloudVect.normalized, planetCameraVect.normalized) + 0.85f, 0, 0.2f);
+			cloudScale = Mathf.Clamp (Vector3.Dot(planetCloudVect.normalized, planetCameraVect.normalized) + cloudOverHang, 0, 0.2f) / 1.25f;
 			transform.localScale = new Vector3 (cloudScale, cloudScale, cloudScale);
 		}
 	}
